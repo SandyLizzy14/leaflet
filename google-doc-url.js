10c82c24c7470ab53590a66dbbeda06fc76bbd9a
SandyLizzy14 <68877427+SandyLizzy14@users.noreply.github.com>
2020-10-20 17:34:14 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1CElxBWjH7hFfjJoLv5PjxtdX92WaTPN0DG8dVlXiVkg/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyDizCz2bMUlFJzw4mVv2xTIYpwYUN5MjwA';
